59fa597d5400d3effdd66df5e76d08747f7e9903
acfoard <acfoard@gmail.com>
2019-08-23 14:59:27 -0400

    game logic

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -5,3 +5,23 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, 'clear'])
   )
+
+
+export const checkCollision = (player, stage, {x: moveX, y: moveY}) => {
+  for (let y=0; y<player.tetromino.length; y += 1) {
+    for (let x=0; x<player.tetromino[y].length; x += 1) {
+      //check that on tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+        //check movement inside game height
+        !stage[y + player.pos.y + moveY] ||
+        //check movement inside game width
+        !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+        //check that cell moveing to isn't set to clear
+        stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear') {
+          return true;
+        }
+      }
+    }
+  }
+};
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -58,7 +58,7 @@ export const TETROMINOS = {
     },
 }
 
-export const reandomTetromino = () => {
+export const randomTetromino = () => {
     const tetrominos = 'IJLOSTZ';
     const randTetromino = 
         tetrominos[Math.floor(Math.random()*tetrominos.length)];
