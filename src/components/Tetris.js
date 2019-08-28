667a595b5593f2312b24a2a1279bbdb904ab7f34
acfoard <acfoard@gmail.com>
2019-08-28 11:51:51 -0400

    done game

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,8 +6,10 @@ import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 //custom hooks
+import { useInterval } from '../hooks/useInterval';
 import {usePlayer} from '../hooks/usePlayer';
 import {useStage} from '../hooks/useStage';
+import {useGameStatus} from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage';
@@ -20,7 +22,8 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared)
 
   console.log('re-render')
 
@@ -33,11 +36,20 @@ const Tetris = () => {
   const startGame = () => {
     //reset everything
     setStage(createStage());
+    setDroptime(1000);
     resetPlayer();
     setGameOver(false);
+    setScore(0);
+    setLevel(0);
+    setRows(0);
   }
 
   const drop = () => {
+    //increase level after 10 rows cleared
+    if(rows > (level +1) * 10) {
+      setLevel(prev => prev + 1);
+      setDroptime(1000 / (level + 1) + 200);
+    }
     if (!checkCollision( player, stage, { x:0, y:1})) {
       updatePlayerPos({x:0, y:1, collided: false}); 
     } else {
@@ -51,8 +63,18 @@ const Tetris = () => {
     }     
   }
 
+  const keyUp = ({keyCode}) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDroptime(1000 / (level + 1) + 200);
+      }
+    }
+  }
+
   const dropPlayer = () => {
-    drop();    
+    setDroptime(null);
+    drop();
+
   }
 
   const move = ({keyCode}) => {
@@ -69,8 +91,12 @@ const Tetris = () => {
      }    
   }
 
+  useInterval(() => {
+    drop();
+  }, dropTime)
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e=> move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e=> move(e)} onKeyUp={keyUp}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -78,9 +104,9 @@ const Tetris = () => {
           <Display gameOver = {gameOver} text={"Game Over"} />
         :
           <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
+            <Display text={`Score: ${score}`} />
+            <Display text={`Level: ${level}`} />
+            <Display text={`Rows: ${rows}`} />
           </div>
           }
           <StartButton callback={startGame} />
